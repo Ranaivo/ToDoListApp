aad584b6708b6adb9b585c00e0aa76d143f83355
Ranaivo <mahenina@gmail.com>
2025-10-01 20:56:29 +0300

    Check box

--- app.js
+++ app.js
@@ -6,6 +6,22 @@ form.addEventListener('submit', function(event) {
     event.preventDefault();
     const li = document.createElement('li');
     li.textContent = input.value;
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+
+// Add event listener to checkbox to toggle line-through style
+    checkbox.addEventListener('change', function() {
+        if (checkbox.checked) {
+            li.style.textDecoration = 'line-through';
+            li.style.fontStyle = 'italic';
+        } else {
+            li.style.textDecoration = 'none';
+            li.style.fontStyle = 'normal';
+        }
+    }); 
+    
+    li.appendChild(checkbox);
+    input.value = '';
     const deleteButton = document.createElement('button');
     deleteButton.textContent = 'Remove';
     li.appendChild(deleteButton);
