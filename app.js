59bc507e3d56aa5a0666963d9b0a1753aa3ac4d9
Ranaivo <mahenina@gmail.com>
2025-10-01 17:12:56 +0300

    Add basic JS to add tasks

---
+++ app.js
@@ -0,0 +1,11 @@
+const form = document.querySelector('form');
+const input = document.querySelector('#taskInput');
+const ul = document.querySelector('#tasks');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const li = document.createElement('li');
+    li.textContent = input.value;
+    ul.appendChild(li);
+    input.value = '';
+});
