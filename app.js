d41ab05eef0ff2675bb6e09bd2b0e7b7ab329f0a
Ranaivo <mahenina@gmail.com>
2025-10-01 18:18:02 +0300

    Add remove task feature

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ form.addEventListener('submit', function(event) {
     event.preventDefault();
     const li = document.createElement('li');
     li.textContent = input.value;
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Remove';
+    li.appendChild(deleteButton);
     ul.appendChild(li);
     input.value = '';
+    deleteButton.addEventListener('click', function() {
+        if (li.parentNode === ul) {
+            ul.removeChild(li);
+        }
+    }); 
 });
